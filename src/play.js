e4e4ea31cf91ec5b595e6e45b3ef7b5f3cbad3a5
Kosina Zoltán <kosinaz@gmail.com>
2018-09-21 18:38:16 +0200

    Added burger and eating

--- src/play.js
+++ src/play.js
@@ -4,40 +4,37 @@ var playState = {
 
     game.over = false;
 
-    
+
     game.levelMusic.fadeTo(500, 0.3);
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.gravity.y = 1000;
 
-    game.mouth = game.add.sprite(980, 288, 'sprites');
-    game.mouth.animations.add('eat', [
-      'mouth_lit_64_1',
-      'mouth_lit_64_2',
-      'mouth_lit_64_3',
-      'mouth_lit_64_4',
-      'mouth_lit_64_3',
-      'mouth_lit_64_2',
-      'mouth_lit_64_1'
+    game.lips = game.add.sprite(980, 288, 'sprites');
+    game.lips.animations.add('eat', [
+      'lips1',
+      'lips2',
+      'lips3',
+      'lips4',
+      'lips3',
+      'lips2',
+      'lips1'
     ], 30, true);
-    game.mouth.animations.play('eat');
+    game.lips.animations.play('eat');
 
     game.foods = game.add.group();
-    var food = game.foods.create(
-      10, game.rnd.integerInRange(150, 426), 'sprites', 'spam_lit'
-    );
-    
-    game.physics.enable([game.mouth, game.foods], Phaser.Physics.ARCADE);
-    game.mouth.body.collideWorldBounds = true;
-    game.mouth.body.immovable = true;
-    game.mouth.body.onWorldBounds = new Phaser.Signal();
-    game.mouth.body.onWorldBounds.add(function () {
+
+    game.physics.enable(game.lips, Phaser.Physics.ARCADE);
+    game.lips.body.collideWorldBounds = true;
+    game.lips.body.immovable = true;
+    game.lips.body.onWorldBounds = new Phaser.Signal();
+    game.lips.body.onWorldBounds.add(function () {
       if (game.over) {
         return;
       }
       game.over = true;
-      game.mouth.animations.stop('eat');
-      game.mouth.loadTexture('sprites', 'mouth_lit_64_4');
+      game.lips.animations.stop('eat');
+      game.lips.loadTexture('sprites', 'lips4');
       game.levelMusic.fadeTo(500, 0.01);
       var restartButton = game.add.text(512, 288, 'Restart', game.style);
       restartButton.inputEnabled = true;
@@ -47,27 +44,36 @@ var playState = {
       game.add.audio('fail', 0.3).play();
     });
 
-    food.body.velocity.setTo(500, 0);
-    food.body.allowGravity = false;
-    food.body.immovable = true;
-    
+    game.time.events.loop(1000, function () {
+      if (game.over) {
+        return;
+      }
+      var food = game.foods.create(
+        10, game.rnd.integerInRange(150, 426), 'sprites', 'burger'
+      );
+      game.physics.enable(food, Phaser.Physics.ARCADE);
+      food.body.velocity.setTo(400 + game.rnd.integerInRange(0, 200), 0);
+      food.body.allowGravity = false;
+      food.body.immovable = true;
+    });
+
     game.input.mouse.start();
     game.input.mouse.mouseUpCallback = function () {
-      if (game.over || game.mouth.y < 150) {
+      if (game.over || game.lips.y < 150) {
         return;
       }
       game.add.audio('boing' + game.rnd.integerInRange(1, 3), 0.3).play();
-      game.mouth.body.velocity.setTo(0, -400);
+      game.lips.body.velocity.setTo(0, -400);
     }
 
   },
 
   update: function () {
-    game.physics.arcade.collide(game.mouth, game.foods, function (mouth, food) {
+    game.physics.arcade.collide(game.lips, game.foods, function (lips, food) {
       game.add.tween(food.scale).to({
         x: 0,
         y: 0
-      }, 100, Phaser.Easing.Circular.In, true);
+      }, 50, Phaser.Easing.Circular.In, true);
     });
   }
 

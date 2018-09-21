c010d8bc443180921e2e7992a0fc66770061e18c
Kosina Zoltán <kosinaz@gmail.com>
2018-09-21 17:56:18 +0200

    Added food

--- src/play.js
+++ src/play.js
@@ -4,14 +4,7 @@ var playState = {
 
     game.over = false;
 
-    game.input.mouse.start();
-    game.input.mouse.mouseUpCallback = function () {
-      if (game.over || game.mouth.y < 150) {
-        return;
-      }
-      game.add.audio('boing' + game.rnd.integerInRange(1, 3), 0.3).play();
-      game.mouth.body.velocity.setTo(0, -400);
-    }    
+    
     game.levelMusic.fadeTo(500, 0.3);
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -28,8 +21,15 @@ var playState = {
       'mouth_lit_64_1'
     ], 30, true);
     game.mouth.animations.play('eat');
-    game.physics.enable(game.mouth, Phaser.Physics.ARCADE);
+
+    game.foods = game.add.group();
+    var food = game.foods.create(
+      10, game.rnd.integerInRange(150, 426), 'sprites', 'spam_lit'
+    );
+    
+    game.physics.enable([game.mouth, game.foods], Phaser.Physics.ARCADE);
     game.mouth.body.collideWorldBounds = true;
+    game.mouth.body.immovable = true;
     game.mouth.body.onWorldBounds = new Phaser.Signal();
     game.mouth.body.onWorldBounds.add(function () {
       if (game.over) {
@@ -47,10 +47,28 @@ var playState = {
       game.add.audio('fail', 0.3).play();
     });
 
+    food.body.velocity.setTo(500, 0);
+    food.body.allowGravity = false;
+    food.body.immovable = true;
+    
+    game.input.mouse.start();
+    game.input.mouse.mouseUpCallback = function () {
+      if (game.over || game.mouth.y < 150) {
+        return;
+      }
+      game.add.audio('boing' + game.rnd.integerInRange(1, 3), 0.3).play();
+      game.mouth.body.velocity.setTo(0, -400);
+    }
+
   },
 
   update: function () {
-
+    game.physics.arcade.collide(game.mouth, game.foods, function (mouth, food) {
+      game.add.tween(food.scale).to({
+        x: 0,
+        y: 0
+      }, 100, Phaser.Easing.Circular.In, true);
+    });
   }
 
 }

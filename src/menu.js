ae7e9a8a7d1e4d409b00e953266a24eaf4e35226
Kosina Zoltán <kosinaz@gmail.com>
2018-09-21 13:26:03 +0200

    Added animated title and mouth

--- src/menu.js
+++ src/menu.js
@@ -2,8 +2,15 @@ var menuState = {
 
   create: function () {
 
-    game.add.sprite(512, 288, 'sprites', 'title');
-    var subtitle = game.add.sprite(512, 288, 'sprites', 'regurgitated_lit');
+    game.stage.backgroundColor = '#66b4e2';
+    var title, subtitle;
+    game.add.sprite(512, 160, 'sprites', 'title_off');
+    title = game.add.sprite(512, 160, 'sprites', 'title');
+    title.alpha = 0;
+    game.add.tween(title).to({
+      alpha: 1
+    }, 500, Phaser.Easing.Bounce.Out, true, 500);
+    subtitle = game.add.sprite(512, 288, 'sprites', 'subtitle');
     subtitle.scale = {
       x: 0,
       y: 0
@@ -11,15 +18,15 @@ var menuState = {
     game.add.tween(subtitle.scale).to({
       x: 1,
       y: 1
-    }, 1000, Phaser.Easing.Bounce.Out, true, 500);
+    }, 1000, Phaser.Easing.Bounce.Out, true, 1500);
     game.add.tween(subtitle).to({
-      y: 438
-    }, 1000, Phaser.Easing.Bounce.Out, true, 500);
+      y: 388
+    }, 1000, Phaser.Easing.Bounce.Out, true, 1500);
 
     /**
      * Set the Start New Game button
      */
-    this.startNewGameButton = game.add.text(512, 528, 'Start New Game', game.style);
+    this.startNewGameButton = game.add.text(512, 488, 'Start New Game', game.style);
     this.startNewGameButton.inputEnabled = true;
     this.startNewGameButton.events.onInputUp.add(this.start, this);
     this.startNewGameButton.anchor.set(0.5, 0.5);
--- src/play.js
+++ src/play.js
@@ -1,11 +1,22 @@
 var playState = {
 
-    create: function () {
-        
-    },
+  create: function () {
+    game.stage.backgroundColor = '#66b4e2';
+    var mouth = game.add.sprite(980, 288, 'sprites');
+    mouth.animations.add('eat', [
+      'mouth_lit_64_1',
+      'mouth_lit_64_2',
+      'mouth_lit_64_3',
+      'mouth_lit_64_4',
+      'mouth_lit_64_3',
+      'mouth_lit_64_2',
+      'mouth_lit_64_1'
+    ], 30, true);
+    mouth.animations.play('eat');
+  },
 
-    update: function () {  
+  update: function () {
 
-    }
+  }
 
 }
